161ff5c316a34e798c102153a7bed558c084368f
Rycer <szymkris5@gmail.com>
2023-11-24 16:25:59 +0100

    Add youtube search, remove fileSystemCheck, change the input look

--- linux_source_code.c
+++ linux_source_code.c
@@ -7,7 +7,7 @@ void mainMenu();
 void fileSystemCheck();
 void search();
 void helpCommand();
-void enemyFileSearchHandler(const char *enemy_path);
+void enemyFileSearchHandler(const char *enemy_path, const char *championToSearch, const char *enemyToSearch);
 void createRepo();
 const int CURRENT_CHAMPION_AMOUNT = 166;
 
@@ -15,100 +15,29 @@ const char THE_MAIN_PATH[] = "main_notes_folder/";
 const char THE_NOTES_POSTFIX[] = "_notes/";
 const char THE_TXT_POSTFIX[] = ".txt";
 const char UNDERSCOREVSUNDERSCORE[] = "_vs_";
+const char YT_SEARCH_QUERY[] = "https://www.youtube.com/results?search_query=";
 
 int main(){
 
 printf("Program start successful\n");
 printf("Type \"help\" for the command list\n");
+printf("Make sure you read the manual for a good experience\n");
 mainMenu();
 
 return 0;
 }
 
-void fileSystemCheck()
-{
-FILE *filepointer;
-filepointer = fopen("champ_list.txt", "r");
-    
-    if (filepointer == NULL)
-    {
-        printf("Failed to open the champ_list file!\n");
-        printf("Check if the file is in the program's directory.\n");
-        printf("If not, run the createChampList command in the main menu.\n");
-        printf("If it is in the directory, reopen the program and try again.\n");
-        printf("If this fails, submit a ticket on Github, at https://github.com/Rycerzklanu/search_engine_for_champnotes");
-
-        mainMenu();
-        return;
-    }
-    if (filepointer != NULL)
-    {
-        printf("Opened the champ_list file successfully...\n");
-        char champToCheck[50];
-        char helperCheckPath[300];
-        char trueCheckPath[400];
-        FILE *champnotespointer; 
-        DIR *directorypointerFileSystemCheck;
-        struct dirent *entry;
-        directorypointerFileSystemCheck = opendir("/main_notes_folder");
-        if (directorypointerFileSystemCheck == NULL) 
-        {
-            printf("Cannot open the main_notes_folder directory!\n");
-            mainMenu();
-            return;
-        }
-
-        if (directorypointerFileSystemCheck != NULL)
-        {
-            while((entry = readdir(directorypointerFileSystemCheck)) != NULL)
-            {
-                fgets(champToCheck, 50, filepointer);
-                //the quit function functionality
-                if (strcmp(champToCheck, "quit") == 0)
-                {
-                    printf("Exiting program\n");
-                    return;
-                }
- 
-                else if (strcmp(champToCheck, "back") == 0)
-                {
-                    mainMenu();
-                    return;
-                }
-                snprintf(helperCheckPath, 300, "%s%s", THE_MAIN_PATH, champToCheck);
-                snprintf(trueCheckPath, 400, "%s%s", helperCheckPath, THE_NOTES_POSTFIX);
-                printf("%s\n", trueCheckPath);
-
-
-    
-        
-            }
-            mainMenu();
-            return;
-        }
-        
-
-    }
-    
-
-}
-
 void mainMenu()
 {
     char command[100];
-    printf("You are in the main menu\n");
-    printf("Enter a command:\n");
+    printf("\nYou are in the main menu\n");
+    printf("Enter a command: ");
     scanf("%s", command);
     if (strcmp(command, "search") == 0)
     {
         search();
         return;
     }    
-    else if (strcmp(command, "check") == 0)
-    {
-        fileSystemCheck();
-        return;
-    }
     else if (strcmp(command, "help") == 0)
     { 
         helpCommand();
@@ -116,7 +45,7 @@ void mainMenu()
     }
     else if (strcmp(command, "quit") == 0)
     {
-        printf("Exiting program\n");
+        printf("\nExiting program\n");
         return;
     }
     else if (strcmp(command, "createrepo") == 0)
@@ -126,7 +55,7 @@ void mainMenu()
     }
     else 
     {
-        printf("Not a known command! Type \"help\" to see the command list!\n");
+        printf("\nNot a known command! Type \"help\" to see the command list!\n");
         mainMenu();
         return;
     }
@@ -135,11 +64,11 @@ void search()
 {
     char champToSearch[20];
     
-    printf("Type the name of the champion\n");
+    printf("Type the name of the champion: ");
     scanf("%s", champToSearch);
     if (strcmp(champToSearch, "quit") == 0)
     {
-        printf("Exiting program\n");
+        printf("\nExiting program\n");
         return;
     }
     
@@ -148,6 +77,7 @@ void search()
         mainMenu();
         return;
     }
+
     char helperSearchPath[50];
     char trueSearchPath[100];
     snprintf(helperSearchPath, 50, "%s%s", THE_MAIN_PATH, champToSearch);
@@ -158,7 +88,7 @@ void search()
     directorypointerSearch = opendir(trueSearchPath);
         if (directorypointerSearch == NULL) 
         {
-            printf("Cannot open the directory!\n");
+            printf("\nCannot open the directory!\n");
             mainMenu();
             return;
         }
@@ -170,20 +100,19 @@ void search()
             char helperEnemySearchPath[200];
             char noPostfixEnemySearchPath[250];
             char trueEnemySearchPath[300];
-            printf("Directory opened successfully\n");
-            printf("Type the name of the enemy champion:\n");
+            printf("\nType the name of the enemy champion: ");
             scanf("%s", enemyName);
             if (strcmp(enemyName, "quit") == 0)
             {
-                printf("Exiting program\n");
+                printf("\nExiting program\n");
                 return;
             }
             snprintf(helperEnemySearchPath, 200, "%s%s%s", trueSearchPath, champToSearch, UNDERSCOREVSUNDERSCORE);
             snprintf(noPostfixEnemySearchPath, 250, "%s%s", helperEnemySearchPath, enemyName);
             snprintf(trueEnemySearchPath, 300, "%s%s", noPostfixEnemySearchPath, THE_TXT_POSTFIX);
-            printf("%s\n", trueEnemySearchPath);
+            printf("\n%s\n", trueEnemySearchPath);
             
-            enemyFileSearchHandler(trueEnemySearchPath);
+            enemyFileSearchHandler(trueEnemySearchPath, champToSearch, enemyName);
             return;
 
             //file name format : [champname]_vs_[enemychampname].txt            
@@ -202,17 +131,17 @@ void helpCommand()
 }
 
 
-void enemyFileSearchHandler(const char *enemy_path)
+void enemyFileSearchHandler(const char *enemy_path, const char *championToSearch, const char *enemyToSearch)
 {
     FILE *enemypointer;
     
     char toDisplayOrToOpen[10];
     char enemyFileReader[10000];
-    printf("Do you want to display the contents or open the file in the default editor [d/o]?\n");
+    printf("Do you want to display the contents, open the file, or search a VOD on Youtube? [d/o/s]? ");
     scanf("%9s", toDisplayOrToOpen);
     if (strcmp(toDisplayOrToOpen, "quit") == 0)
     {
-        printf("Exiting program\n");
+        printf("\nExiting program\n");
         return;
     }
     else if (strcmp(toDisplayOrToOpen, "back") == 0)
@@ -227,14 +156,14 @@ void enemyFileSearchHandler(const char *enemy_path)
         enemypointer = fopen(enemy_path, "r");
         if (enemypointer == NULL)
         {
-            perror("Error opening file");
+            perror("\nError opening file");
             exit(EXIT_FAILURE);
         }
         while(fgets(enemyFileReader, 10000, enemypointer) != NULL)
         {
             printf("%s", enemyFileReader);
         }
-        printf("Search successful, returning to the search menu\n");
+        printf("\nSearch successful, returning to the search menu\n");
         search();
         return;
     } 
@@ -248,14 +177,30 @@ void enemyFileSearchHandler(const char *enemy_path)
         int fileOpenStatus = system(fullFilePathToOpen);
 
         if (fileOpenStatus == -1)
-            perror("Error executing the command!\n");
+            perror("\nError executing the command!\n");
         else if (fileOpenStatus != 0)
         {
-            printf("Command failed with exit status %d\n", fileOpenStatus);
+            printf("\nCommand failed with exit status %d\n", fileOpenStatus);
             return;
         }
     }
+    
+    else if(strcmp(toDisplayOrToOpen, "s") == 0)
+    {
+        char youtubeSearchPath[100];
+        snprintf(youtubeSearchPath, 100, "%s%s%s%s%s%s", "xdg-open ", YT_SEARCH_QUERY, championToSearch, "+vs+", enemyToSearch, "+challenger+grandmaster");
+         int ytSearchStatus = system(youtubeSearchPath);
+         freopen("/dev/null", "w", stderr); 
 
+        if (ytSearchStatus == -1)
+            perror("\nError executing the command!\n");
+        else if (ytSearchStatus != 0)
+        {
+            printf("\nCommand failed with exit status %d\n", ytSearchStatus);
+            return;
+        }
+
+    }
     search();
     return;
 
@@ -263,15 +208,15 @@ void enemyFileSearchHandler(const char *enemy_path)
 
 void createRepo()
 {
-    printf("Creating the repository, do not close the program\n");
+    printf("\nCreating the repository, do not close the program\n");
     int status = system("scripts/createRepo.sh");
     if (status == -1)
     {
-        perror("Error creating the repo\n");
+        perror("\nError creating the repo\n");
     }
     else if (status != 0)
     {
-        printf("Command failed with exit status %d\n", status);
+        printf("\nCommand failed with exit status %d\n", status);
     }
     mainMenu();
     return;
